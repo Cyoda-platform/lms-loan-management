e2ff477b881718a855c9aaf4d1b0a794b4bc51ac
Paul Schleger <paul@cyoda.com>
2025-10-07 16:16:24 -0700

    add missing ProcessorDefinition and ProcessorDefinitionConstants

---
+++ src/main/java/org/cyoda/cloud/api/event/common/statemachine/conf/ProcessorDefinition.java
@@ -0,0 +1,52 @@
+package org.cyoda.cloud.api.event.common.statemachine.conf;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * Base class for the generated processor definitions.
+ *<p>
+ * jsonschema2pojo doesn't support the generation of discriminator-related annotations, so this is the workaround
+ * and we just using 'existingJavaType' in json-schema to declare inheritance.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "type",
+        "name"
+})
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type", // when null, we should use ExternalizedProcessorDefinition
+        defaultImpl = ExternalizedProcessorDefinition.class
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = ExternalizedProcessorDefinition.class, name = ProcessorDefinitionConstants.EXTERNALIZED_PROCESSOR_TYPE),
+        @JsonSubTypes.Type(value = ScheduledTransitionProcessorDefinition.class, name = ProcessorDefinitionConstants.SCHEDULED_PROCESSOR_TYPE)
+})
+public abstract class ProcessorDefinition {
+
+    @JsonProperty("type")
+    public String getType() {
+        return null;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        // do nothing by default
+    }
+
+    @JsonProperty("name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/org/cyoda/cloud/api/event/common/statemachine/conf/ProcessorDefinitionConstants.java
@@ -0,0 +1,12 @@
+package org.cyoda.cloud.api.event.common.statemachine.conf;
+
+public final class ProcessorDefinitionConstants {
+    
+    public static final String EXTERNALIZED_PROCESSOR_TYPE = "externalized";
+    public static final String SCHEDULED_PROCESSOR_TYPE = "scheduled";
+    
+    private ProcessorDefinitionConstants() {
+        // Utility class - prevent instantiation
+    }
+}
+
